31cdf40d125498d1a3cffe017b0285291b920b76
davidokun <davidokun99@gmail.com>
2017-01-31 22:42:57 -0500

    Scopes and Immediately Invoked Functions Expressions.

---
+++ basic-javascript/scopes.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression.
+(function (lastname) {
+    var firstname = 'John';
+    console.log(firstname);
+    console.log(lastname);
+
+}('Doe'));
+
+var firstname = 'Jane';
+console.log(firstname);
