008c17ca696602b7d0eba27f9df36a773db78006
davidokun <davidokun99@gmail.com>
2017-02-09 18:44:45 -0500

    Using .json file to retrieve data as a javascript object and use its properties.

--- node-modules/greets/spanish.js
+++ node-modules/greets/spanish.js
@@ -1,5 +1,8 @@
+// Require a .json file which will be imported as javascript object.
+var greetings = require('./greetings.json');
+
 var greet = function () {
-    console.log("Hola");
+    console.log(greetings.es); // Use object properties
 };
 
 module.exports = greet;
