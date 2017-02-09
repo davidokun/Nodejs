4958b689c62abc254226427b397e236e443ab4de
david.cuellar <david.x.cuellar.-nd@disney.com>
2017-02-09 18:36:45 -0500

    Using multi-module configuration.

---
+++ node-modules/appMultiGreet.js
@@ -0,0 +1,7 @@
+// If greets.js does not exists, it looks for a folder called greet and then
+// for a file called index.js.
+var greet = require('./greets');
+
+// Call each method exported from the modules english.js and spanish.js
+greet.english();
+greet.spanish();
