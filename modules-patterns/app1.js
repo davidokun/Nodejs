8d543579dc25f139b155099e468c579977c4763a
davidokun <davidokun99@gmail.com>
2017-02-10 17:12:45 -0500

    Third Module Pattern. Adding object to module.exports as a function constructor.

--- modules-patterns/app1.js
+++ modules-patterns/app1.js
@@ -4,3 +4,15 @@ greet();
 // Extraction the specific function from module.exports through require function.
 var greet2 = require('./greet2').greet;
 greet2();
+
+// Gets an object from the require function.
+var greet3 = require('./greet3');
+greet3.greet();
+
+// Change function on object retrieved
+greet3.greeting = 'Changed hello world! form Greet3';
+
+// Call require again on greet3
+var greet3b = require('./greet3');
+// Call changed message. Require caches objects.
+greet3b.greet();
