9809f7d8abf86a8765dac8c3e7378a60e23fceee
davidokun <davidokun99@gmail.com>
2017-02-28 21:15:01 -0500

    Custom Event Emitter in Javascript.

---
+++ custom-event-emitter/app.js
@@ -0,0 +1,20 @@
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('greet', function () {
+    console.log('Somewhere, someone said hello.');
+});
+
+emtr.on('greet', function () {
+    console.log('A greeting occurred!.');
+});
+
+console.log('Hello');
+emtr.emit('greet');
+
+emtr.on('click', function () {
+    console.log('Some click happened');
+});
+
+emtr.emit('click');
