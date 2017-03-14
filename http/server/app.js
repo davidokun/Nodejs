f2d6781ad0cbb2b95d1846b7a4a33a74be93f2de
davidokun <davidokun99@gmail.com>
2017-03-13 19:03:01 -0500

    Creating a Server.

---
+++ http/server/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Node\n');
+
+}).listen(8080, '127.0.0.1');
