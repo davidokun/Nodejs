75ddc069ebf435e79b660767a369c10a22f248d6
davidokun <davidokun99@gmail.com>
2017-03-29 19:41:01 -0500

    Using some kind of routing.

---
+++ http/routing/app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    if (req.url === '/') {
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        fs.createReadStream(__dirname + '/index.html')
+            .pipe(res);
+    } else if (req.url === '/person') {
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+
+        const person = {
+            firstName: 'Jane',
+            lastName: 'Doe'
+        };
+
+        res.end(JSON.stringify(person));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+
+}).listen(8080);
