3215bdf937f3f10f8235d6ffcb4306415f57e42c
davidokun <davidokun99@gmail.com>
2017-04-12 21:35:01 -0500

    Using ES6 template literals to concatenate strings.

---
+++ ecamas6-template-literals/app.js
@@ -0,0 +1,10 @@
+var name = 'John Doe';
+
+// Concatenate using +
+var greet = 'Hello ' + name;
+
+// Using ES6 template literals
+var greet2 = `Hello ${name}`;
+
+console.log(greet);
+console.log(greet2);
