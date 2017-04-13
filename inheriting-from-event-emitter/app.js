365494d95b92a09b21df38eb4a94d06c9f718ad4
davidokun <davidokun99@gmail.com>
2017-04-12 21:15:01 -0500

    Inheriting from EventEmitter with util.inherits from util module.

---
+++ inheriting-from-event-emitter/app.js
@@ -0,0 +1,21 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greet() {
+    this.greeting = 'Hello World!';
+}
+
+util.inherits(Greet, EventEmitter);
+
+Greet.prototype.greet = function (data) {
+    console.log(this.greeting + ':' + data);
+    this.emit('greet', data);
+};
+
+var greeter1 = new Greet();
+
+greeter1.on('greet', function (data) {
+    console.log('Someone greeted: ' + data);
+});
+
+greeter1.greet('John');
