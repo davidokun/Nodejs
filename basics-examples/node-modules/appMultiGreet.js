7de70c19c21c3da3968c7546153be71177620495
davidokun <davidokun99@gmail.com>
2017-04-14 21:52:01 -0500

    Change project structure. Adding basic examples folder.

--- basics-examples/node-modules/appMultiGreet.js
+++ basics-examples/node-modules/appMultiGreet.js
@@ -1,6 +1,6 @@
 // If greets.js does not exists, it looks for a folder called greet and then
 // for a file called index.js.
-var greet = require('./greets');
+var greet = require('./greets/index');
 
 // Call each method exported from the modules english.js and spanish.js
 greet.english();
