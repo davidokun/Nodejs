b7fd928e3a673e59e6fa739846b459db42a4f356
davidokun <davidokun99@gmail.com>
2017-04-18 20:22:01 -0500

    Using Static Files and Express middleware.

--- express/express-test-app-1/app.js
+++ express/express-test-app-1/app.js
@@ -3,10 +3,20 @@ const app = express();
 
 // Get environment variables
 const port = process.env.PORT || 3000;
+app.listen(port);
+
+// Middleware calls
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) {
+    console.log('Request URL: ' + req.url);
+    next();
+});
 
 app.get('/', function (req, res) {
-    res.send("<html><head></head><body><h1>Hello Express!! Yikess!!</h1></body></html>");
+    res.send("<html><head><link href=assets/styles.css type='text/css' rel='stylesheet' /></head><body><h1>Hello Express!! Yikess!!</h1></body></html>");
 });
+
 
 app.get('/api', function (req, res) {
     res.json({name: 'John', last: 'Doe'})
@@ -17,6 +27,3 @@ app.get('/api', function (req, res) {
 app.get('/person/:id', function (req, res) {
     res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
 });
-
-
-app.listen(port);
