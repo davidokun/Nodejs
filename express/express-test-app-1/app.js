f4c8f8c70a1b8e2e8d574db2a7ce03fc590062d9
davidokun <davidokun99@gmail.com>
2017-04-18 20:15:01 -0500

    Using Routes with express.

--- express/express-test-app-1/app.js
+++ express/express-test-app-1/app.js
@@ -12,4 +12,11 @@ app.get('/api', function (req, res) {
     res.json({name: 'John', last: 'Doe'})
 });
 
+
+// Routing
+app.get('/person/:id', function (req, res) {
+    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
+
 app.listen(port);
