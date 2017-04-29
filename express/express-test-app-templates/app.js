6e0537a20c613baed93091b1f8cd5a89d4c0927b
davidokun <davidokun99@gmail.com>
2017-04-28 20:35:01 -0500

    Querystring and Post Parameters.

--- express/express-test-app-templates/app.js
+++ express/express-test-app-templates/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+const urlencoderParser = bodyParser.urlencoded({extended: false});
+
+const jsonParser = bodyParser.json();
 
 app.listen(3001);
 
@@ -13,6 +18,18 @@ app.get('/', function (req, res) {
 });
 
 app.get('/person/:id', function (req, res) {
-    res.render('person', {ID: req.params.id});
+    res.render('person', {ID: req.params.id, Qstr: req.query.qstr});
+});
+
+app.post('/person', urlencoderParser ,function (req, res) {
+    res.send('Thank you');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+});
+
+app.post('/personjson', jsonParser,function (req, res) {
+    res.send('Thank you');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
 });
 
